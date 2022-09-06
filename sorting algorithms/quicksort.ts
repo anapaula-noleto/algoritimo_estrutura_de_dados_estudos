01f62c90f40ef6dbb76142253fadc50417af4974
Ana Paula Nolêto <apaulanazarenonoleto@gmail.com>
2022-09-06 14:45:39 -0300

    create quicksort file

---
+++ sorting algorithms/quicksort.ts
@@ -0,0 +1,4 @@
+// Algoritimos DC são recursivos
+// Descubra o caso-base que deve ser o mais simples possível
+// Divida o problema em subproblemas menores até que ele se torne o caso base
+// A cada recursão o problema deve ser menor
